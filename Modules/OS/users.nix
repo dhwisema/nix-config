405c1c56a077aa41e3a77482797be1fed19ce07e
dhwisema <dhwisema@ncsu.edu>
2026-01-29 19:10:20 -0500

    up[date

--- Modules/OS/users.nix
+++ Modules/OS/users.nix
@@ -55,8 +55,10 @@
       extraGroups = if role == "server" then[
         "networkmanager"
         "wheel"
-        "tty"
-      ] else ["networkmanager" "wheel" "plugdev"];
+      ] else ["networkmanager"
+        "wheel"
+         "plugdev"
+       "dialout"];
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFWcxSaxxRfizmQh2zdrRCt0Ic+eVKOc2w+nsQOVBh+e irrelevancy@Jester"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINVkFiDUTkFwLQ3g13XgdN5ekhmNYo5whYn+JmjTvLYu huntingdog5forsteam@gmail.com" #beau
