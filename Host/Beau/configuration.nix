4c0585edb041b1501ddc2f06d8cf8ad3e42a310c
dhwisema <dhwisema@ncsu.edu>
2026-02-19 00:07:34 -0500

    switch to normal release set\

--- Host/Beau/configuration.nix
+++ Host/Beau/configuration.nix
@@ -3,7 +3,7 @@
   lib,
   ...
 }: {
-  boot.kernelPackages = pkgs.linuxPackages_lts;
+  boot.kernelPackages = pkgs.linuxPackages;
   systemd.services."systemd-suspend" = {
     serviceConfig = {
       Environment = ''"SYSTEMD_SLEEP_FREEZE_USER_SESSIONS=false"'';
