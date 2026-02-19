39d51e185a6f03489b3ea1ed66ab490a8d0acce4
dhwisema <dhwisema@ncsu.edu>
2026-02-19 00:04:44 -0500

    use lts until nvidia fix

--- Host/Beau/configuration.nix
+++ Host/Beau/configuration.nix
@@ -3,6 +3,7 @@
   lib,
   ...
 }: {
+  boot.kernelPackages = pkgs.linuxPackages_lts;
   systemd.services."systemd-suspend" = {
     serviceConfig = {
       Environment = ''"SYSTEMD_SLEEP_FREEZE_USER_SESSIONS=false"'';
