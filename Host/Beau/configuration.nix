e9f97c47be9fa75cd076c25b80a29056cf565ecc
dhwisema <dhwisema@ncsu.edu>
2026-03-22 22:37:36 -0400

    udev

--- Host/Beau/configuration.nix
+++ Host/Beau/configuration.nix
@@ -9,4 +9,9 @@
       Environment = ''"SYSTEMD_SLEEP_FREEZE_USER_SESSIONS=false"'';
     };
   };
+  services.udev.extraRules = ''
+  # Royal Kludge RK R65 - Disable joystick/tablet nodes to fix Steam detection
+  SUBSYSTEM=="input", ATTRS{idVendor}=="342d", ATTRS{idProduct}=="e453", ENV{ID_INPUT_JOYSTICK}="", ENV{ID_INPUT_TABLET}=""
+'';
+
 }
