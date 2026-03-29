27037be4ee97b74729e583390b4c485fbfd0573f
dhwisema <dhwisema@ncsu.edu>
2026-03-28 22:22:21 -0400

    fuckery

--- Home/common/programs/home-pkgs-desktop.nix
+++ Home/common/programs/home-pkgs-desktop.nix
@@ -35,9 +35,6 @@
     systemd = {
       enable = true;
       autoStart = true;
-      environment = {
-        USE_LAYER_SHELL = 1;
-      };
     };
     settings = {
       close_on_focus_loss = true;
