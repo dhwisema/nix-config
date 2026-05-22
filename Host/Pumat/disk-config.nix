43ca0310d5c4e17b42d7744dcef7d4aecd5f1995
Howard Wiseman <112828283+dhwisema@users.noreply.github.com>
2026-05-21 23:24:34 -0400

    Update disk configuration

--- Host/Pumat/disk-config.nix
+++ Host/Pumat/disk-config.nix
@@ -8,7 +8,7 @@
           type = "gpt";
           partitions = {
             boot = {
-              size = "1024M";
+              size = "1G";
               type = "EF00";
               content = {
                 type = "filesystem";
@@ -17,20 +17,100 @@
                 mountpoint = "/boot";
               };
             };
-            plainSwap = {
-              size = "32G";
-              content = {
-                type = "swap";
-                discardPolicy = "both";
-                resumeDevice = true; # resume from hiberation from this device
-              };
-            };
             root = {
               size = "100%";
+                  content = {
+                  type = "btrfs";
+                  extraArgs = [
+                    "-L"
+                    "nixos"
+                    "-f"
+                  ];
+                  subvolumes = {
+                    "/root" = {
+                      mountpoint = "/";
+                      mountOptions = [
+                        "subvol=root"
+                        "compress=zstd:3"
+                        "noatime"
+                        "ssd"
+                        "discard=async"
+                      ];
+                    };
+                    "/home" = {
+                      mountpoint = "/home";
+                      mountOptions = [
+                        "subvol=home"
+                        "compress=zstd:3"
+                        "noatime"
+                        "ssd"
+                        "discard=async"
+                      ];
+                    };
+                    "/nix" = {
+                      mountpoint = "/nix";
+                      mountOptions = [
+                        "subvol=nix"
+                        "compress=zstd:3"
+                        "noatime"
+                        "ssd"
+                        "discard=async"
+                      ];
+                    };
+                    "/swap" = {
+                      mountpoint = "/swap";
+                      mountOptions = [
+                        "subvol=swap"
+                        "compress=zstd:3"
+                        "compress=no"
+                        "ssd"
+                      ];
+                      swap.swapfile.size = "32G";
+                    };
+                  };
+                };
+            };
+          };
+        };
+      }; #other here
+      sata-one = {
+        type = "disk";
+        device = "/dev/sdb";
+        content = {
+          type = "gpt";
+          partitions = {
+            empty = {
+              size = "100%";
+            };
+          };
+
+        }
+
+      };
+      sata-two = {
+        type = "disl";
+        device = "/dev/sda";
+        content = {
+          type = "gpt";
+          partitions = {
+            data = {
+              size = "100%";
               content = {
-                type = "filesystem";
-                format = "ext4";
-                mountpoint = "/";
+                type = "btrfs";
+                extraArgs = {
+                  "-f"
+                  "-L"
+                  "-m raid1"
+                  "-d single"
+                  "/dev/sda1"
+                };
+                mountpoint = "/data";
+                mountOptions = [
+                  "ssd_spread"
+                  "compress=zstd:3"
+                  "noatime"
+                  "discard=async"
+                ];
               };
             };
           };
