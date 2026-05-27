34061e7cf63a79d9a7f6b1745c0db2fb9cf65019
dhwisema <dhwisema@ncsu.edu>
2026-05-26 23:01:26 -0400

    pumat hardware accel

--- Host/Pumat/default.nix
+++ Host/Pumat/default.nix
@@ -5,6 +5,20 @@
   ...
 }:
 {
+
+  hardware.graphics = {
+    enable = true;
+
+    extraPackages = with pkgs; [
+      intel-media-driver
+      intel-compute-runtime
+      vpl-gpu-rt
+    ];
+  };
+
+  environment.sessionVariables = {
+    LIBVA_DRIVER_NAME = "iHD";
+  };
   imports = [
     ./hardware-configureation.nix
   ];
