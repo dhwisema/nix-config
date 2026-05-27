4d5dbbc63be0c6466ef858724d20019633866b56
dhwisema <dhwisema@ncsu.edu>
2026-05-26 23:15:15 -0400

    jellyfin pumat and remove dead nix

--- Modules/Containers/Jellyfin.nix
+++ Modules/Containers/Jellyfin.nix
@@ -1,10 +1,11 @@
 {pkgs,config,...}:{
   services.jellyfin = {
     enable = true;
+    cacheDir = "/data/cache/jellyfin";
+    logDir = /data/log/jellyfin;
+    configDir = /data/config/jellyfin;
   };
  
-
-
   systemd.services.jellyfin.environment = {
     LIBVA_DRIVER_NAME = "iHD";
   };
