00983237f0161d5c89b109fd39fb1b2e214a721d
dhwisema <dhwisema@ncsu.edu>
2026-06-04 10:51:59 -0400

    add librepods config

---
+++ Modules/Desktop/Airpods-fix.nix
@@ -0,0 +1,13 @@
+{}:{
+  hardware.bluetooth = {
+  enable = true;
+  powerOnBoot = true;
+  settings = {
+    General = {
+      # Spoof Apple Vendor ID for advanced AirPods features
+      DeviceID = "bluetooth:004C:0000:0000";
+    };
+  };
+};
+environment.systemPackages = with pkgs; [librepods];
+}
