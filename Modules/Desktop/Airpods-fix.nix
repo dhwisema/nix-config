d6bfbdffcb21663a382ff14f4bc8ea364e9f513f
dhwisema <dhwisema@ncsu.edu>
2026-06-07 02:35:35 -0400

    librepods

--- Modules/Desktop/Airpods-fix.nix
+++ Modules/Desktop/Airpods-fix.nix
@@ -1,13 +1,19 @@
-{pkgs, config, lib,...}:{
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}:
+{
   hardware.bluetooth = {
-  enable = true;
-  powerOnBoot = true;
-  settings = {
-    General = {
-      # Spoof Apple Vendor ID for advanced AirPods features
-      DeviceID = "bluetooth:004C:0000:0000";
+    enable = true;
+    powerOnBoot = true;
+    settings = {
+      General = {
+        # Spoof Apple Vendor ID for advanced AirPods features
+        DeviceID = "bluetooth:004C:0000:0000";
+      };
     };
   };
-};
-environment.systemPackages = with pkgs; [librepods];
+  environment.systemPackages = with pkgs; [ librepods ];
 }
