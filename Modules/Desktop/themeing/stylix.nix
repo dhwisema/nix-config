2ebd2dc33a91e951d608b17b94117f4c59a83964
dhwisema <dhwisema@ncsu.edu>
2026-06-07 02:35:48 -0400

    stylix fix?

--- Modules/Desktop/themeing/stylix.nix
+++ Modules/Desktop/themeing/stylix.nix
@@ -11,7 +11,7 @@
   stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/everforest-dark-hard.yaml";
   stylix.polarity = "dark";
   stylix.autoEnable = true;
-  stylix.targets.qt.platform = lib.mkForce "qtct" ;
+  stylix.targets.qt.platform = lib.mkForce "qtct";
   stylix.cursor = {
     name = "BreezeX-RosePine-Linux";
     package = pkgs.rose-pine-cursor;
