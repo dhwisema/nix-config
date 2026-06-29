18c2963d424059894e40efa528af1980896a2c4a
dhwisema <dhwisema@ncsu.edu>
2026-06-29 12:54:55 -0400

    waveforms causing build fail temporarily remove

--- flake.nix
+++ flake.nix
@@ -84,8 +84,8 @@
                 nix-flatpak.nixosModules.nix-flatpak
                 stylix.nixosModules.stylix
                 niri.nixosModules.niri
-                waveforms.nixosModule
-                ({ users.users.${username}.extraGroups = [ "plugdev" ]; })
+               # waveforms.nixosModule
+               # ({ users.users.${username}.extraGroups = [ "plugdev" ]; })
                 nixos-hardware.nixosModules.common-cpu-amd # sets scheduling things for kernel
                 nixos-hardware.nixosModules.common-pc-ssd # ssd trim
                 noctalia-greeter.nixosModules.default 
