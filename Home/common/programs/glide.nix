c1c6e6150fef62d2b032e015275fbb1860b227ab
dhwisema <dhwisema@ncsu.edu>
2026-07-13 13:20:31 -0400

    test2

--- Home/common/programs/glide.nix
+++ Home/common/programs/glide.nix
@@ -1,4 +1,5 @@
 {inputs,config,pkgs,...}:{
+ imports = [inputs.glide-browser.homeManagerModules.default];
  home.packages = [inputs.glide-browser.packages.${pkgs.system}.default];
  programs.glide-browser.enable = true;
 }
